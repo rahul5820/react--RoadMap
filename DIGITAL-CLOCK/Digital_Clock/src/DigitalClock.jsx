0eac29632e2049f4e0f119782e24052e49ec7896
Rahul <Rr8305820@gmail.com>
2025-08-12 22:12:21 +0530

    updating React js RoadMap

---
+++ DIGITAL-CLOCK/Digital_Clock/src/DigitalClock.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+function DigitalClock() {
+  const [time, SetTime] = useState(new Date());
+
+
+  useEffect(()=>{
+    const intervalId = setInterval(()=>{
+        SetTime(new Date())
+    },1000)
+
+    console.log(new Date());
+
+    return(()=>{
+        clearInterval(intervalId)
+    })
+    
+
+  },[])
+
+
+
+  function formatTime(){
+    let Hours=time.getHours();
+    const Minutes=time.getMinutes();
+    const Seconds=time.getSeconds();
+    const Meridiem = Hours>12?"PM":"AM";
+
+
+    Hours = Hours%12||12;
+
+     return `${padZero(Hours)}:${padZero(Minutes)}:${padZero(Seconds)}:${padZero(Meridiem)}`
+
+
+  }
+  function padZero(Numbers){
+    
+   return(Numbers<10?"0":"")+  Numbers;
+
+  }
+
+
+  return (
+    <>
+      <div className=" Digital-Clock-Container">
+        <div className="Digital-Clock">
+          <span>{formatTime()}</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DigitalClock;
